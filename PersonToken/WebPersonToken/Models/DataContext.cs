6b1af0428446d7b1493d22b58892967ad1ee91a3
Diego Peredo <a2019118858@estudiantes.upsa.edu.bo>
2021-04-29 18:18:14 -0400

    Se agregó el controlador MVC de people

---
+++ PersonToken/WebPersonToken/Models/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebPersonToken.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<WebPersonToken.Models.Person> People { get; set; }
+    }
+}
